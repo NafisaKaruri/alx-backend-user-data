d751504693f44f3220bf81e2aa7abe0aea6dfa48
NafisaKaruri <nafeesahassan016@gmail.com>
2024-11-06 23:25:23 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,12 +3,14 @@
 This module provides utilities for logging user data while redacting sensitive
 Personally Identifiable Information (PII) and securely hashing passwords.
 """
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Returns the log message obfuscated"""
     for i in fields:
         message = re.sub(f'{i}=.*?{separator}',
-                        f'{i}={redaction}{separator}', message)
+                         f'{i}={redaction}{separator}', message)
     return message
