419129afbd2ae920e83cb4838d29356a4823b2c3
NafisaKaruri <nafeesahassan016@gmail.com>
2024-11-06 23:34:01 +0000

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,25 @@ This module provides utilities for logging user data while redacting sensitive
 Personally Identifiable Information (PII) and securely hashing passwords.
 """
 from typing import List
+import logging
 import re
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, ';')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
