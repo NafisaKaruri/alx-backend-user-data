baa881b5743cea8357009f3f060feef22a26c109
NafisaKaruri <nafeesahassan016@gmail.com>
2024-11-06 23:37:30 +0000

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,10 +17,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """initialize"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """format"""
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION, message, ';')
 
