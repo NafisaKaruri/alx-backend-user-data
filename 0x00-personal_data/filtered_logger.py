7c14213eff1d9099c35c7ee70a0d6438f8baf8a6
NafisaKaruri <nafeesahassan016@gmail.com>
2024-11-07 01:59:59 +0000

    done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -37,11 +37,18 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    if db_name is None:
+        raise ValueError("Database name not set in environment variables")
 
-    con = mysql.connector.connect(user=db_username,
-                                  password=db_password,
-                                  host=db_host,
-                                  database=db_name)
+    try:
+        con = mysql.connector.connect(user=db_username,
+                                      password=db_password,
+                                      host=db_host,
+                                      database=db_name)
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        raise
+
     return con
 
 
